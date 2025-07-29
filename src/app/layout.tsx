926759e0eab161d81f1be6b986b2d92456ff10dc
Suren Poghosyan <suren.poghosian.18@gmail.com>
2025-07-29 13:59:54 +0400

    feat: add google meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="gbR2rvkLF2zzx5ybmttbsa1zl6ZonK3ZdSP1c1tY5uc" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
